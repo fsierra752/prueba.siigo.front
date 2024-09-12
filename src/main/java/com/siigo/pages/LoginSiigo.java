3327bb9d2acdbe462d24c85596687f14962daca8
fsierra752 <fredysierra949@gmail.com>
2024-09-11 22:39:49 -0500

    se realiza implementacion para hacer login en pagina

--- src/main/java/com/siigo/pages/LoginSiigo.java
+++ src/main/java/com/siigo/pages/LoginSiigo.java
@@ -1,16 +1,12 @@
 package com.siigo.pages;
 
+import com.siigo.interactions.Esperar;
 import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.NoSuchElementException;
-
 
 public class LoginSiigo extends PageObject {
 
@@ -20,37 +16,28 @@ public class LoginSiigo extends PageObject {
         this.driver = driver;
     }
 
-
-    private WebElement getShadowRootElement(String shadowHostSelector) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), 10);
-        WebElement shadowHost = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(shadowHostSelector)));
-        System.out.println("Shadow Host: " + shadowHost);
-        if (shadowHost == null) {
-            throw new NoSuchElementException("No se encontró el elemento con el selector: " + shadowHostSelector);
-        }
-        //Usar JavaScript para obtener el shadowRoot
-        WebElement shadowRoot = (WebElement) ((JavascriptExecutor) getDriver()).executeScript("return arguments[0].shadowRoot", shadowHost);
-        System.out.println("Shadow Root: " + shadowRoot);
-        return shadowRoot;
-    }
-
-    public WebElement getInputField() {
-        WebElement shadowRoot = getShadowRootElement("input-atom#username");
-        return shadowRoot.findElement(By.cssSelector("#username-input"));
-    }
-
     public void enterUsername(String username) {
-        WebElement inputElement = getInputField();
-        inputElement.sendKeys(username);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebElement pagina = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login")));
+        WebElement parentElement = driver.findElement(By.className("login"));
+        WebElement imputName = parentElement.findElement(By.id("username"));
+        imputName.click();
+        imputName.sendKeys(username);
+        Esperar.unMomento(1);
     }
 
-    public static final Target INPUT_USER_NAME = Target.the("Campo para ingresar usuario")
-            .located(By.name("username-input"));
+    public void enterPassword(String password) {
+        WebElement parentElement = driver.findElement(By.className("login"));
+        WebElement imputPassword = parentElement.findElement(By.name("password-input"));
+        imputPassword.click();
+        imputPassword.sendKeys(password);
+        Esperar.unMomento(1);
+    }
 
-    public static final Target INPUT_USER_PASSWORD = Target.the("Campo para ingresar contraseña")
-            .located(By.id("password"));
-
-    public static final Target BTN_INGRESAR = Target.the("Boton ingresar a siigo")
-            .located(By.id("login-submit"));
+    public void clicBotonIngresar() {
+        WebElement parentElement = driver.findElement(By.className("login"));
+        WebElement btnIngresar = parentElement.findElement(By.id("login-submit"));
+        btnIngresar.click();
+    }
 
 }
